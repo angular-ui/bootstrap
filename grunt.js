8899523c60dac0507b1ccf7fd7d73c9e6e7f4290
Andy Joslin <andytjoslin@gmail.com>
2012-10-05 14:29:43 -0400

    Add grunt with stubbed out "test-single" task

---
+++ grunt.js
@@ -0,0 +1,40 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    test: {
+      files: ['test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint test'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true
+      },
+      globals: {}
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint test');
+
+  grunt.registerTask('test-single', 'run tests', function() {
+    
+  });
+
+};
