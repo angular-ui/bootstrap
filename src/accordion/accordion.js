dbd7aa3153f33e74102baa801777f7cff99a8a39
Pete Bacon Darwin <pete@bacondarwin.com>
2012-12-17 14:52:48 +0000

    feat(accordion): Transition and attribute improvements
    Add support for open and close CSS transitions.
    Add support for optional is-open attribute on <accordion-group> directive
    Remove isolate scope from <accordion> directive

--- src/accordion/accordion.js
+++ src/accordion/accordion.js
@@ -1,79 +1,159 @@
-angular.module('ui.bootstrap.accordion', []);
+angular.module('ui.bootstrap.accordion', ['ui.bootstrap.transition']);
 angular.module('ui.bootstrap.accordion').controller('AccordionController', ['$scope', function ($scope) {
 
-  var groups = $scope.groups = [];
+  // This array keeps track of the accordion groups
+  this.groups = [];
 
-  this.select = function(group) {
-    angular.forEach(groups, function (group) {
-      group.selected = false;
+  // Ensure that all the groups in this accordion are closed
+  this.closeOthers = function(openGroup) {
+    angular.forEach(this.groups, function (group) {
+      if ( group !== openGroup ) {
+        group.close();
+      }
     });
-    group.selected = true;
+  };
+  
+  // This is called from the accordion-group directive to add itself to the accordion
+  this.addGroup = function(groupScope) {
+    var that = this;
+    this.groups.push(groupScope);
+    groupScope.groups = this.groups;
+    groupScope.$on('$destroy', function (event) {
+      that.removeGroup(groupScope);
+    });
   };
 
-  this.toggle = function(group) {
-    if (group.selected) {
-      group.selected = false;
-    } else {
-      this.select(group);
-    }
-  };
-
-  this.addGroup = function(group) {
-    groups.push(group);
-    if(group.selected) {
-      this.select(group);
-    }
-  };
-
+  // This is called from the accordion-group directive when to remove itself
   this.removeGroup = function(group) {
-    groups.splice(groups.indexOf(group), 1);
+    var index = this.groups.indexOf(group);
+    if ( index !== -1 ) {
+      this.groups.splice(this.groups.indexOf(group), 1);
+    }
   };
 
 }]);
 
-/* accordion: Bootstrap accordion implementation
- * @example
- <accordion>
-   <accordion-group title="sth">Static content</accordion-group>
-   <accordion-group title="sth">Static content - is it? {{sth}}</accordion-group>
-   <accordion-group title="group.title" ng-repeat="group in groups">{{group.content}}</accordion-group>
- </accordion>
- */
+// The accordion directive simply sets up the directive controller
+// and adds an accordion CSS class to itself element.
 angular.module('ui.bootstrap.accordion').directive('accordion', function () {
   return {
     restrict:'E',
-    transclude:true,
-    scope:{},
     controller:'AccordionController',
-    templateUrl:'template/accordion/accordion.html'
+    link: function(scope, element) {
+      element.addClass('accordion');
+    }
   };
 });
 
-angular.module('ui.bootstrap.accordion').directive('accordionGroup', function () {
+// The accordion-group directive indicates a block of html that will expand and collapse in an accordion
+angular.module('ui.bootstrap.accordion').directive('accordionGroup', ['$parse', '$transition', '$timeout', function($parse, $transition, $timeout) {
+  // We need to find the div that contains the body element of each accordion group so that we can get its height.
+  // This is so that the animations work, since height: auto doesn't trigger the CSS animation.
+  // Unfortunately jqLite element.find() cannot find by CSS class.
+  var findByClass = function(parentElement, cssClass) {
+    var element;
+    angular.forEach(parentElement.find('div'), function(div) {
+      if ( angular.element(div).hasClass(cssClass) ) {
+        element = div;
+      }
+    });
+    return angular.element(element);
+  };
+
+  // CSS transitions don't work with height: auto, so we have to manually change the height to a specific
+  // value and then once the animation completes, we can reset the height to auto.
+  // Unfortunately if you do this while the CSS transitions are specified (i.e. in the CSS class "collapse")
+  // then you trigger a change to height 0 in between.
+  // The fix is to remove the "collapse" CSS class while changing the height back to auto - phew!
+  var fixUpHeight = function(scope, bodyElement, height) {
+    // We remove the collapse CSS class to prevent a transition when we change to height: auto
+    bodyElement.removeClass('collapse');
+    bodyElement.css({ height: height });
+    // It appears that by reading offsetWidth, the browser realises that we have changed the height already :-/
+    var x = bodyElement[0].offsetWidth;
+    bodyElement.addClass('collapse');
+  };
+
   return {
-    require:'^accordion',
-    restrict:'E',
-    transclude:true,
-    scope:{
-      heading:'@'
-    },
+    require:'^accordion',         // We need this directive to be inside an accordion
+    restrict:'E',                 // It will be an element
+    transclude:true,              // It transcludes the contents of the directive into the template
+    replace: true,                // The element containing the directive will be replaced with the template
+    templateUrl:'template/accordion/accordion-group.html',
+    scope:{ heading:'@' },        // Create an isolated scope and interpolate the heading attribute onto this scope
     link: function(scope, element, attrs, accordionCtrl) {
 
-      accordionCtrl.addGroup(scope);
+      var bodyElement = findByClass(element, 'accordion-body');
+      var currentTransition;
+      var doTransition = function(change) {
+        if ( currentTransition ) {
+          currentTransition.cancel();
+        }
+        currentTransition = $transition(bodyElement,change);
+        currentTransition.then(function() { currentTransition = undefined; }, function() { currentTransition = undefined; } );
+        return currentTransition;
+      };
 
-      scope.select = function() {
-        accordionCtrl.select(scope);
+      accordionCtrl.addGroup(scope);
+      
+      scope.open = function() {
+        accordionCtrl.closeOthers(scope);
+
+        doTransition({ height : bodyElement[0].scrollHeight + 'px' })
+        .then(function() {
+          // This check ensures that we don't accidentally update the height if the user has closed the group
+          // while the animation was still running
+          if ( scope.isOpen ) {
+            fixUpHeight(scope, bodyElement, 'auto');
+          }
+        });
+        scope.isOpen = true;
+      };
+      
+      scope.close = function() {
+        if ( scope.isOpen ) {
+          scope.isOpen = false;
+          fixUpHeight(scope, bodyElement, bodyElement[0].scrollHeight + 'px');
+
+          doTransition({'height':'0'});
+        }
       };
 
       scope.toggle = function() {
-        accordionCtrl.toggle(scope);
+        if ( scope.isOpen ) {
+          scope.close();
+        } else {
+          scope.open();
+        }
       };
 
-      scope.$on('$destroy', function (event) {
-        accordionCtrl.removeGroup(scope);
-      });
-    },
-    templateUrl:'template/accordion/accordion-group.html',
-    replace:true
+      scope.isOpen = false;
+      
+      scope.height = function() {
+        return bodyElement.css('height');
+      };
+
+      var getOpen, setOpen, updateOpen;
+      updateOpen = function(value) {
+        scope.isOpen = value;
+        if ( value ) {
+          scope.open();
+        } else {
+          scope.close();
+        }
+      };
+      
+      if ( attrs.isOpen ) {
+        getOpen = $parse(attrs.isOpen);
+        setOpen = getOpen.assign;
+        scope.$watch( function() {
+          return getOpen(scope.$parent);
+        }, updateOpen);
+        scope.$watch('isOpen', function(value) {
+          setOpen(scope.$parent, value);
+        });
+        updateOpen(scope.$eval(attrs.isOpen));
+      }
+    }
   };
-});
+}]);
--- src/accordion/test/accordionSpec.js
+++ src/accordion/test/accordionSpec.js
@@ -1,64 +1,92 @@
+// Mock up $transition service
+angular.module('mockTransition', []).factory('$transition', function() {
+  $transition = jasmine.createSpy('$transition').andReturn( jasmine.createSpyObj('transition promise', ['then', 'cancel'])).andCallThrough();
+  return $transition;
+});
+
 describe('accordion', function () {
+  var $scope;
 
-  var scope, $compile, $controller;
+  beforeEach(module('mockTransition'));
   beforeEach(module('ui.bootstrap.accordion'));
-  beforeEach(module('template/accordion/accordion.html', 'template/accordion/accordion-group.html'));
+  beforeEach(module('template/accordion/accordion-group.html'));
 
-  beforeEach(inject(function(_$rootScope_, _$compile_, _$controller_) {
-    scope = _$rootScope_;
-    $compile = _$compile_;
-    $controller = _$controller_;
+  beforeEach(inject(function ($rootScope) {
+    $scope = $rootScope;
   }));
 
-  describe('accordion controller', function () {
+  describe('controller', function () {
 
     var ctrl;
-    beforeEach(function () {
-      ctrl = $controller('AccordionController', {$scope:scope});
+    beforeEach(inject(function($controller) {
+      ctrl = $controller('AccordionController', { $scope: $scope });
+    }));
+
+    describe('addGroup', function() {
+      it('adds a the specified group to the collection', function() {
+        var group1, group2;
+        ctrl.addGroup(group1 = $scope.$new());
+        ctrl.addGroup(group2 = $scope.$new());
+        expect(ctrl.groups.length).toBe(2);
+        expect(ctrl.groups[0]).toBe(group1);
+        expect(ctrl.groups[1]).toBe(group2);
+      });
     });
 
-    it('should allow group selection', function () {
-      var first = {}, second = {};
-      ctrl.addGroup(first);
-      ctrl.addGroup(second);
-
-      expect(first.selected).toBeFalsy();
-      expect(second.selected).toBeFalsy();
-
-      ctrl.select(second);
-      expect(first.selected).toBeFalsy();
-      expect(second.selected).toBeTruthy();
-
-      ctrl.select(first);
-      expect(first.selected).toBeTruthy();
-      expect(second.selected).toBeFalsy();
+    describe('closeOthers', function() {
+      it('should close other groups', function() {
+        var group1, group2, group3;
+        ctrl.addGroup(group1 = jasmine.createSpyObj('group1', ['close', '$on']));
+        ctrl.addGroup(group2 = jasmine.createSpyObj('group2', ['close', '$on']));
+        ctrl.addGroup(group3 = jasmine.createSpyObj('group3', ['close', '$on']));
+        ctrl.closeOthers(group2);
+        expect(group1.close).toHaveBeenCalled();
+        expect(group2.close).not.toHaveBeenCalled();
+        expect(group3.close).toHaveBeenCalled();
+      });
     });
 
-    it('it should un-select selected groups when a new selected group is added', function () {
-      var first = {selected:true}, second = {selected:true};
-      ctrl.addGroup(first);
-      ctrl.addGroup(second);
-
-      expect(first.selected).toBeFalsy();
-      expect(second.selected).toBeTruthy();
-    });
-
-    it('should ignore remove of non-existing group', function () {
-      ctrl.removeGroup({});
+    describe('removeGroup', function() {
+      it('should remove the specified group', function () {
+        var group1, group2, group3;
+        ctrl.addGroup(group1 = $scope.$new());
+        ctrl.addGroup(group2 = $scope.$new());
+        ctrl.addGroup(group3 = $scope.$new());
+        ctrl.removeGroup(group2);
+        expect(ctrl.groups.length).toBe(2);
+        expect(ctrl.groups[0]).toBe(group1);
+        expect(ctrl.groups[1]).toBe(group3);
+      });
+      it('should ignore remove of non-existing group', function () {
+        var group1, group2;
+        ctrl.addGroup(group1 = $scope.$new());
+        ctrl.addGroup(group2 = $scope.$new());
+        expect(ctrl.groups.length).toBe(2);
+        ctrl.removeGroup({});
+        expect(ctrl.groups.length).toBe(2);
+      });
     });
   });
 
-  describe('accordion DOM manipulations', function () {
+  describe('accordion-group', function () {
+
+    var scope, $compile, $timeout, $transition;
+
+    beforeEach(inject(function(_$rootScope_, _$compile_, _$timeout_) {
+      scope = _$rootScope_;
+      $compile = _$compile_;
+      $timeout = _$timeout_;
+    }));
 
     var element, groups;
     var findGroupLink = function (index) {
       return groups.eq(index).find('a').eq(0);
     };
     var findGroupBody = function (index) {
-      return groups.eq(index).find('div.accordion-body').eq(0);
+      return groups.eq(index).find('.accordion-body').eq(0);
     };
 
-    describe('static accordion', function () {
+    describe('with static groups', function () {
       beforeEach(function () {
         var tpl =
           "<accordion>" +
@@ -66,13 +94,16 @@ describe('accordion', function () {
             "<accordion-group heading=\"title 2\">Content 2</accordion-group>" +
             "</accordion>";
         element = angular.element(tpl);
+        angular.element(document.body).append(element);
         $compile(element)(scope);
         scope.$digest();
-        groups = element.find('div.accordion div.accordion-group');
+        groups = element.find('.accordion-group');
+      });
+      afterEach(function() {
+        element.remove();
       });
 
       it('should create accordion groups with content', function () {
-
         expect(groups.length).toEqual(2);
         expect(findGroupLink(0).text()).toEqual('title 1');
         expect(findGroupBody(0).text().trim()).toEqual('Content 1');
@@ -81,51 +112,56 @@ describe('accordion', function () {
       });
 
       it('should change selected element on click', function () {
-
         findGroupLink(0).click();
-        expect(findGroupBody(0)).toHaveClass('in');
-        expect(findGroupBody(1)).not.toHaveClass('in');
+        $timeout.flush();
+        scope.$digest();
+        expect(findGroupBody(0).height()).toBeGreaterThan(0);
 
         findGroupLink(1).click();
-        expect(findGroupBody(0)).not.toHaveClass('in');
-        expect(findGroupBody(1)).toHaveClass('in');
+        $timeout.flush();
+        scope.$digest();
+        expect(findGroupBody(1).height()).toBeGreaterThan(0);
+        expect(findGroupBody(0).height()).toBe(0);
       });
 
       it('should toggle element on click', function() {
         findGroupLink(0).click();
-        expect(findGroupBody(0)).toHaveClass('in');
+        $timeout.flush();
+        scope.$digest();
+        expect(findGroupBody(0).height()).toBeGreaterThan(0);
         findGroupLink(0).click();
-        expect(findGroupBody(0)).not.toHaveClass('in');
+        $timeout.flush();
+        scope.$digest();
+        expect(findGroupBody(0).height()).toBe(0);
       });
     });
 
-    describe('dynamic accordion', function () {
-
-      var element, model;
+    describe('with dynamic groups', function () {
+      var model;
       beforeEach(function () {
         var tpl =
           "<accordion>" +
             "<accordion-group ng-repeat='group in groups' heading='{{group.name}}'>{{group.content}}</accordion-group>" +
-            "</accordion>";
+          "</accordion>";
         element = angular.element(tpl);
         model = [
           {name: 'title 1', content: 'Content 1'},
           {name: 'title 2', content: 'Content 2'}
         ];
-      });
-
-      it('should generate accordion groups using ng-repeat', function () {
 
         $compile(element)(scope);
         scope.$digest();
+      });
 
-        groups = element.find('div.accordion div.accordion-group');
+      it('should have no groups initially', function () {
+        groups = element.find('.accordion-group');
         expect(groups.length).toEqual(0);
+      });
 
-        scope.$apply(function(){
-           scope.groups = model;
-        });
-        groups = element.find('div.accordion div.accordion-group');
+      it('should have a group for each model item', function() {
+        scope.groups = model;
+        scope.$digest();
+        groups = element.find('.accordion-group');
         expect(groups.length).toEqual(2);
         expect(findGroupLink(0).text()).toEqual('title 1');
         expect(findGroupBody(0).text().trim()).toEqual('Content 1');
@@ -135,17 +171,37 @@ describe('accordion', function () {
 
       it('should react properly on removing items from the model', function () {
         scope.groups = model;
-        $compile(element)(scope);
-
         scope.$digest();
-        groups = element.find('div.accordion div.accordion-group');
+        groups = element.find('.accordion-group');
         expect(groups.length).toEqual(2);
 
         scope.groups.splice(0,1);
         scope.$digest();
-        groups = element.find('div.accordion div.accordion-group');
+        groups = element.find('.accordion-group');
         expect(groups.length).toEqual(1);
       });
+    });
+
+    describe('isOpen attribute', function() {
+      beforeEach(function () {
+        var tpl =
+          "<accordion>" +
+            "<accordion-group title=\"'title 1'\" is-open=\"open1\">Content 1</accordion-group>" +
+            "<accordion-group title=\"'title 2'\" is-open=\"open2\">Content 2</accordion-group>" +
+            "</accordion>";
+        element = angular.element(tpl);
+        scope.open1 = false;
+        scope.open2 = true;
+        $compile(element)(scope);
+        scope.$digest();
+        $timeout.flush();
+        groups = element.find('.accordion-group');
+      });
+
+      it('should open the group with isOpen set to true', function () {
+        expect(findGroupBody(0).scope().isOpen).toBe(false);
+        expect(findGroupBody(1).scope().isOpen).toBe(true);
+       });
     });
 
   });
