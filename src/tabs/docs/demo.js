3794506ad03fbcfc2635a68112699ca33ebe5b1d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-12-30 20:31:59 +0100

    demo(tabs): add tabs demo

---
+++ src/tabs/docs/demo.js
@@ -0,0 +1,6 @@
+var TabsDemoCtrl = function ($scope) {
+  $scope.panes = [
+    { title:"Dynamic Title 1", content:"Dynamic content 1" },
+    { title:"Dynamic Title 2", content:"Dynamic content 2" }
+  ];
+};
---
+++ src/tabs/docs/readme.md
@@ -0,0 +1,1 @@
+AngularJS version of the tabs directive.
