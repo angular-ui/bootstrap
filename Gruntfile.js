cd889b60b67cc88ba9c87443019374406259668e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2013-06-24 21:49:03 +0200

    chore(build): split release process into steps

--- Gruntfile.js
+++ Gruntfile.js
@@ -141,14 +141,18 @@ module.exports = function(grunt) {
     shell: {
       //We use %version% and evluate it at run-time, because <%= pkg.version %>
       //is only evaluated once
-      release: [
+      'release-prepare': [
         'grunt before-test after-test',
         'grunt version', //remove "-SNAPSHOT"
-        'grunt changelog',
+        'grunt changelog'
+      ],
+      'release-complete': [
         'git commit CHANGELOG.md package.json -m "chore(release): v%version%"',
-        'git tag %version%',
+        'git tag %version%'
+      ],
+      'release-start': [
         'grunt version:minor:"SNAPSHOT"',
-        'git commit package.json -m "chore(): Starting v%version%"'
+        'git commit package.json -m "chore(release): Starting v%version%"'
       ]
     },
     ngdocs: {
