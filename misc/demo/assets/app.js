c0215c8be943d561234288e7b6e42fba82478415
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2013-09-01 20:18:47 +0200

    chore(build): update custom build to use $modal service
    Closes #927

--- misc/demo/assets/app.js
+++ misc/demo/assets/app.js
@@ -1,34 +1,39 @@
-
 angular.module('bootstrapDemoApp', ['ui.bootstrap', 'plunker']);
 
-function MainCtrl($scope, $http, $document, orderByFilter) {
+function MainCtrl($scope, $http, $document, $modal, orderByFilter) {
   var url = "http://50.116.42.77:3001";
-  $scope.selectedModules = [];
   //iFrame for downloading
   var $iframe = angular.element('<iframe>').css('display','none');
   $document.find('body').append($iframe);
 
   $scope.showBuildModal = function() {
-    $scope.buildModalShown = true;
-    //Load modules if they aren't loaded yet
-    if (!$scope.modules) {
-      $http.get(url + "/api/bootstrap").then(function(response) {
-        $scope.modules = response.data.modules;
-      }, function() {
-        $scope.buildGetErrorText = "Error retrieving build files from server.";
-      });
-    }
-  };
-
-  $scope.downloadBuild = function() {
-    var downloadUrl = url + "/api/bootstrap/download?";
-    angular.forEach($scope.selectedModules, function(module) {
-      downloadUrl += "modules=" + module + "&";
+    var modalInstance = $modal.open({
+      templateUrl: 'buildModal.html',
+      controller: 'SelectModulesCtrl',
+      resolve: {
+        modules: function() {
+          return $http.get(url + "/api/bootstrap").then(function(response) {
+            return response.data.modules;
+          });
+        }
+      }
     });
-    $iframe.attr('src','');
-    $iframe.attr('src', downloadUrl);
-    $scope.buildModalShown = false;
+
+    modalInstance.result.then(function(selectedModules) {
+      var downloadUrl = url + "/api/bootstrap/download?";
+      angular.forEach(selectedModules, function(module) {
+        downloadUrl += "modules=" + module + "&";
+      });
+      $iframe.attr('src','');
+      $iframe.attr('src', downloadUrl);
+    });
   };
+}
+
+var SelectModulesCtrl = function($scope, $modalInstance, modules) {
+
+  $scope.selectedModules = [];
+  $scope.modules = modules;
 
   $scope.selectedChanged = function(module, selected) {
     if (selected) {
@@ -37,4 +42,12 @@ function MainCtrl($scope, $http, $document, orderByFilter) {
       $scope.selectedModules.splice($scope.selectedModules.indexOf(module), 1);
     }
   };
-}
+
+  $scope.downloadBuild = function () {
+    $modalInstance.close($scope.selectedModules);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss();
+  };
+};
