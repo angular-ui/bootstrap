1c231e4f41fd5030de26c754754a3e4860ad30c7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2013-09-08 19:00:35 +0200

    chore(release): v0.6.0

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,53 @@
+angular.module('bootstrapDemoApp', ['ui.bootstrap', 'plunker']);
 
-angular.module('bootstrapDemoApp', ['ui.directives', 'ui.bootstrap', 'plunker']);
-
-function MainCtrl($scope, $http, orderByFilter) {
+function MainCtrl($scope, $http, $document, $modal, orderByFilter) {
   var url = "http://50.116.42.77:3001";
-  $scope.selectedModules = [];
   //iFrame for downloading
-  var $iframe = $("<iframe>").css('display','none').appendTo(document.body);
+  var $iframe = angular.element('<iframe>').css('display','none');
+  $document.find('body').append($iframe);
 
   $scope.showBuildModal = function() {
-    $scope.buildModalShown = true;
-    //Load modules if they aren't loaded yet
-    if (!$scope.modules) {
-      $http.get(url + "/api/bootstrap").then(function(response) {
-        $scope.modules = response.data.modules;
-      }, function() {
-        $scope.buildGetErrorText = "Error retrieving build files from server.";
+    var modalInstance = $modal.open({
+      templateUrl: 'buildModal.html',
+      controller: 'SelectModulesCtrl',
+      resolve: {
+        modules: function() {
+          return $http.get(url + "/api/bootstrap").then(function(response) {
+            return response.data.modules;
+          });
+        }
+      }
+    });
+
+    modalInstance.result.then(function(selectedModules) {
+      var downloadUrl = url + "/api/bootstrap/download?";
+      angular.forEach(selectedModules, function(module) {
+        downloadUrl += "modules=" + module + "&";
       });
+      $iframe.attr('src','');
+      $iframe.attr('src', downloadUrl);
+    });
+  };
+}
+
+var SelectModulesCtrl = function($scope, $modalInstance, modules) {
+
+  $scope.selectedModules = [];
+  $scope.modules = modules;
+
+  $scope.selectedChanged = function(module, selected) {
+    if (selected) {
+      $scope.selectedModules.push(module);
+    } else {
+      $scope.selectedModules.splice($scope.selectedModules.indexOf(module), 1);
     }
   };
 
-  $scope.downloadBuild = function() {
-    var downloadUrl = url + "/api/bootstrap/download?";
-    angular.forEach($scope.selectedModules, function(module) {
-      downloadUrl += "modules=" + module + "&";
-    });
-    $iframe.attr('src','');
-    $iframe.attr('src', downloadUrl);
-    $scope.buildModalShown = false;
+  $scope.downloadBuild = function () {
+    $modalInstance.close($scope.selectedModules);
   };
-}
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss();
+  };
+};
