8691c230987137066619dccfbb5998f0418da527
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2013-12-28 22:42:07 +0100

    fix(demo): correct Bootstrap CSS link in plunks

--- assets/plunker.js
+++ assets/plunker.js
@@ -18,7 +18,7 @@ angular.module('plunker', [])
           '    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/'+ngVersion+'/angular.js"></script>\n' +
           '    <script src="http://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-'+version+'.js"></script>\n' +
           '    <script src="example.js"></script>\n' +
-          '    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/'+bsVersion+'/css/bootstrap-combined.min.css" rel="stylesheet">\n' +
+          '    <link href="//netdna.bootstrapcdn.com/bootstrap/'+bsVersion+'/css/bootstrap.min.css" rel="stylesheet">\n' +
           '  </head>\n' +
           '  <body>\n\n' +
           content + '\n' +
