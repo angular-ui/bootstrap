22fc1177045fc2830a4a69919fb87d78c46f0bf3
Tasos Bekos <tbekos@gmail.com>
2014-01-12 00:38:00 +0100

    refactor(progressbar): remove $transition dependency
     * Remove the need to find out the old value.
     * Reorganize code.
    
    Closes #1564

--- src/progressbar/progressbar.js
+++ src/progressbar/progressbar.js
@@ -1,54 +1,47 @@
-angular.module('ui.bootstrap.progressbar', ['ui.bootstrap.transition'])
+angular.module('ui.bootstrap.progressbar', [])
 
 .constant('progressConfig', {
   animate: true,
   max: 100
 })
 
-.controller('ProgressController', ['$scope', '$attrs', 'progressConfig', '$transition', function($scope, $attrs, progressConfig, $transition) {
+.controller('ProgressController', ['$scope', '$attrs', 'progressConfig', function($scope, $attrs, progressConfig) {
     var self = this,
-        bars = [],
-        max = angular.isDefined($attrs.max) ? $scope.$parent.$eval($attrs.max) : progressConfig.max,
         animate = angular.isDefined($attrs.animate) ? $scope.$parent.$eval($attrs.animate) : progressConfig.animate;
 
+    this.bars = [];
+    this.max = angular.isDefined($attrs.max) ? $scope.$parent.$eval($attrs.max) : progressConfig.max;
+
     this.addBar = function(bar, element) {
-        var oldValue = 0, index = bar.$parent.$index;
-        if ( angular.isDefined(index) &&  bars[index] ) {
-            oldValue = bars[index].value;
+        bar.$element = element;
+
+        if ( !animate ) {
+            bar.$element.css({'transition': 'none'});
         }
-        bars.push(bar);
 
-        this.update(element, bar.value, oldValue);
+        this.bars.push(bar);
 
-        bar.$watch('value', function(value, oldValue) {
-            if (value !== oldValue) {
-                self.update(element, value, oldValue);
-            }
+        bar.$watch('value', function() {
+            self.update( bar );
         });
 
         bar.$on('$destroy', function() {
+            bar.$element = null;
             self.removeBar(bar);
         });
     };
 
-    // Update bar element width
-    this.update = function(element, newValue, oldValue) {
-        var percent = this.getPercentage(newValue);
-
-        if (animate) {
-            element.css('width', this.getPercentage(oldValue) + '%');
-            $transition(element, {width: percent + '%'});
-        } else {
-            element.css({'transition': 'none', 'width': percent + '%'});
-        }
+    this.update = function( bar ) {
+        var percent = this.getPercentage( bar.value );
+        bar.$element.css({ 'width': percent + '%' });
     };
 
     this.removeBar = function(bar) {
-        bars.splice(bars.indexOf(bar), 1);
+        this.bars.splice(this.bars.indexOf(bar), 1);
     };
 
     this.getPercentage = function(value) {
-        return Math.round(100 * value / max);
+        return Math.round(100 * value / this.max);
     };
 }])
 
@@ -60,8 +53,7 @@ angular.module('ui.bootstrap.progressbar', ['ui.bootstrap.transition'])
         controller: 'ProgressController',
         require: 'progress',
         scope: {},
-        template: '<div class="progress" ng-transclude></div>'
-        //templateUrl: 'template/progressbar/progress.html' // Works in AngularJS 1.2
+        templateUrl: 'template/progressbar/progress.html'
     };
 })
 
