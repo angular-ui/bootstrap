a33bac957e829ce28fccdb54fb5478aa3e1c48ae
marcellino <marcellino@webgrid.nl>
2014-01-18 21:04:14 +0100

    chore(jshint): move JSHint configuration to a .jshintrc file
    Closes #1613

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,17 +117,7 @@ module.exports = function(grunt) {
     jshint: {
       files: ['Gruntfile.js','src/**/*.js'],
       options: {
-        curly: true,
-        immed: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        boss: true,
-        eqnull: true,
-        quotmark: 'single',
-        globals: {
-          angular: true
-        }
+        jshintrc: '.jshintrc'
       }
     },
     karma: {
