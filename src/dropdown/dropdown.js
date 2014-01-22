e8d5fefcff794fce28c16c638d9f7baa624c7809
Valentyn Shybanov <vshybanov@twinfield.com>
2014-01-22 18:31:19 +0200

    fix(dropdown): use $animate for adding and removing classes
    Closes #1644

--- src/dropdown/dropdown.js
+++ src/dropdown/dropdown.js
@@ -41,7 +41,7 @@ angular.module('ui.bootstrap.dropdown', [])
   };
 }])
 
-.controller('DropdownController', ['$scope', '$attrs', 'dropdownConfig', 'dropdownService', function($scope, $attrs, dropdownConfig, dropdownService) {
+.controller('DropdownController', ['$scope', '$attrs', 'dropdownConfig', 'dropdownService', '$animate', function($scope, $attrs, dropdownConfig, dropdownService, $animate) {
   var self = this, openClass = dropdownConfig.openClass;
 
   this.init = function( element ) {
@@ -59,7 +59,7 @@ angular.module('ui.bootstrap.dropdown', [])
   };
 
   $scope.$watch('isOpen', function( value ) {
-    self.$element.toggleClass( openClass, value );
+    $animate[value ? 'addClass' : 'removeClass'](self.$element, openClass);
 
     if ( value ) {
       dropdownService.open( $scope );
