4a9dbbef04f4e763aa372a49ec6f81d6b67b5a91
marcellino <marcellino@webgrid.nl>
2014-01-30 22:02:21 +0100

    fix(demo): Scroll to element on initialization

--- misc/demo/assets/app.js
+++ misc/demo/assets/app.js
@@ -1,7 +1,14 @@
 angular.module('bootstrapDemoApp', ['ui.bootstrap', 'plunker', 'ngTouch'], function($httpProvider){
   FastClick.attach(document.body);
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
-});
+}).run(['$location', function($location){
+  //Allows us to navigate to the correct element on initialization
+  if ($location.path() !== '' && $location.path() !== '/') {
+    smoothScroll(document.getElementById($location.path().substring(1)), 500, function(el) {
+      location.replace(el.id);
+    });
+  }
+}]);
 
 var builderUrl = "http://50.116.42.77:3001";
 
