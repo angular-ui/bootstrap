88e7851be705bb7f45fa5ea8107d8ded3b33f97b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-05-01 17:59:12 +0200

    chore(release): v0.11.0

--- assets/app.js
+++ assets/app.js
@@ -1,65 +1,41 @@
-angular.module('bootstrapDemoApp', ['ui.bootstrap', 'plunker'], function($httpProvider){
+angular.module('bootstrapDemoApp', ['ui.bootstrap', 'plunker', 'ngTouch'], function($httpProvider){
   FastClick.attach(document.body);
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
-});
+}).run(['$location', function($location){
+  //Allows us to navigate to the correct element on initialization
+  if ($location.path() !== '' && $location.path() !== '/') {
+    smoothScroll(document.getElementById($location.path().substring(1)), 500, function(el) {
+      location.replace(el.id);
+    });
+  }
+}]);
+
+var builderUrl = "http://50.116.42.77:3001";
 
 function MainCtrl($scope, $http, $document, $modal, orderByFilter) {
-  var url = "http://50.116.42.77:3001";
-  //iFrame for downloading
-  var $iframe = angular.element('<iframe>').css('display','none');
-  $document.find('body').append($iframe);
-
-  var downloadFileFromUrl = function(downloadUrl) {
-    $iframe.attr('src', '');
-    $iframe.attr('src', downloadUrl);
-  };
-  
   $scope.showBuildModal = function() {
     var modalInstance = $modal.open({
       templateUrl: 'buildModal.html',
       controller: 'SelectModulesCtrl',
       resolve: {
         modules: function() {
-          return $http.get(url + "/api/bootstrap").then(function(response) {
+          return $http.get(builderUrl + "/api/bootstrap").then(function(response) {
             return response.data.modules;
           });
         }
       }
     });
-
-    modalInstance.result.then(function(selectedModules) {
-      var downloadUrl = url + "/api/bootstrap/download?";
-      angular.forEach(selectedModules, function(module) {
-        downloadUrl += "modules=" + module + "&";
-      });
-      downloadFileFromUrl(downloadUrl);
-    });
   };
-  
+
   $scope.showDownloadModal = function() {
     var modalInstance = $modal.open({
       templateUrl: 'downloadModal.html',
       controller: 'DownloadCtrl'
     });
-
-    modalInstance.result.then(function(options) {
-      var downloadUrl = ['http://angular-ui.github.io/bootstrap/ui-bootstrap-'];
-      if (options.tpls) {
-        downloadUrl.push('tpls-');
-      }
-      downloadUrl.push(options.version);
-      if (options.minified) {
-        downloadUrl.push('.min');
-      }
-      downloadUrl.push('.js');
-
-      downloadFileFromUrl(downloadUrl.join(''));
-    });
   };
 }
 
 var SelectModulesCtrl = function($scope, $modalInstance, modules) {
-
   $scope.selectedModules = [];
   $scope.modules = modules;
 
@@ -78,6 +54,14 @@ var SelectModulesCtrl = function($scope, $modalInstance, modules) {
   $scope.cancel = function () {
     $modalInstance.dismiss();
   };
+
+  $scope.download = function (selectedModules) {
+    var downloadUrl = builderUrl + "/api/bootstrap/download?";
+    angular.forEach(selectedModules, function(module) {
+      downloadUrl += "modules=" + module + "&";
+    });
+    return downloadUrl;
+  };
 };
 
 var DownloadCtrl = function($scope, $modalInstance) {
@@ -85,12 +69,23 @@ var DownloadCtrl = function($scope, $modalInstance) {
     minified: true,
     tpls: true
   };
-  
+
   $scope.download = function (version) {
-    $scope.options.version = version;
-    $modalInstance.close($scope.options);
+    var options = $scope.options;
+
+    var downloadUrl = ['ui-bootstrap-'];
+    if (options.tpls) {
+      downloadUrl.push('tpls-');
+    }
+    downloadUrl.push(version);
+    if (options.minified) {
+      downloadUrl.push('.min');
+    }
+    downloadUrl.push('.js');
+
+    return downloadUrl.join('');
   };
-  
+
   $scope.cancel = function () {
     $modalInstance.dismiss();
   };
