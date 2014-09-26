1a998c468619695260b49ec577ff41327ddec1f5
Chris Chua <chris.sirhc@gmail.com>
2014-09-26 08:43:57 -0700

    revert(dropdown): remove `C` restrictions to avoid conflicts"
    Breaking change shouldn't go into patch release.
    
    This reverts commit 7512b93fecb6f27df4f5aeba4c756c0c36aebbf2.
    
    Fixes #2757
    
    Fixes #2756

--- src/dropdown/dropdown.js
+++ src/dropdown/dropdown.js
@@ -114,6 +114,7 @@ angular.module('ui.bootstrap.dropdown', [])
 
 .directive('dropdown', function() {
   return {
+    restrict: 'CA',
     controller: 'DropdownController',
     link: function(scope, element, attrs, dropdownCtrl) {
       dropdownCtrl.init( element );
@@ -123,6 +124,7 @@ angular.module('ui.bootstrap.dropdown', [])
 
 .directive('dropdownToggle', function() {
   return {
+    restrict: 'CA',
     require: '?^dropdown',
     link: function(scope, element, attrs, dropdownCtrl) {
       if ( !dropdownCtrl ) {
