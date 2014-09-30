eab6daf64b3c963d8e285e254c75af5f97c24ec1
Kyle <Kylemit@gmail.com>
2014-09-29 22:43:41 -0400

    fix(demo): initial load of fragment URLs
    Closes #2762

--- misc/demo/assets/app.js
+++ misc/demo/assets/app.js
@@ -5,7 +5,7 @@ angular.module('ui.bootstrap.demo', ['ui.bootstrap', 'plunker', 'ngTouch'], func
   //Allows us to navigate to the correct element on initialization
   if ($location.path() !== '' && $location.path() !== '/') {
     smoothScroll(document.getElementById($location.path().substring(1)), 500, function(el) {
-      location.replace(el.id);
+      location.replace('#' + el.id);
     });
   }
 }]);
