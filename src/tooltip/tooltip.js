ff52f52a8d11fa991219160ab293be50d861f902
VitalyVoroshilov <vitekio@yandex.ru>
2014-10-06 12:55:26 +0400

    fix(tooltip): properly gc popupTimeout
    - Make sure to cancel timeout before setting to `null`
    
    Closes #2786

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -203,7 +203,10 @@ angular.module('ui.bootstrap.tooltip', ['ui.bootstrap.position'])
 
             // Show the tooltip popup element.
             function show() {
-              popupTimeout = null;
+              if (popupTimeout) {
+                $timeout.cancel(popupTimeout);
+                popupTimeout = null;
+              }
 
               // If there is a pending remove transition, we must cancel it, lest the
               // tooltip be mysteriously removed.
