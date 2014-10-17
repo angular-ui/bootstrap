170675758770c77e5367ace95d428b6bb70ce0d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-10-17 17:18:16 +0200

    demo(carousel): fix a link to first cat image
    Fixes #2841

--- src/carousel/docs/demo.js
+++ src/carousel/docs/demo.js
@@ -2,7 +2,7 @@ angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($sc
   $scope.myInterval = 5000;
   var slides = $scope.slides = [];
   $scope.addSlide = function() {
-    var newWidth = 600 + slides.length;
+    var newWidth = 600 + slides.length + 1;
     slides.push({
       image: 'http://placekitten.com/' + newWidth + '/300',
       text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
