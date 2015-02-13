ed5013b33157194011452022a9c203e292d389cd
Antoine Pairet <antoine.pairet@b12-consulting.com>
2015-02-13 10:18:43 +0100

    chore(angular): upgrade angular to version 1.3.13

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
   grunt.util.linefeed = '\n';
 
   grunt.initConfig({
-    ngversion: '1.2.16',
+    ngversion: '1.3.13',
     bsversion: '3.1.1',
     modules: [],//to be filled in by build task
     pkg: grunt.file.readJSON('package.json'),
