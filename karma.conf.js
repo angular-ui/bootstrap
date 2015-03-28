fa8e52c406387e1e6548baad3eeca303b738a32e
Chris Chua <chris.sirhc@gmail.com>
2015-03-28 11:30:21 -0700

    chore(build): upgrade to Karma 0.12
    Upgrade configuration file. However, still use an old version of karma-jasmine
    because upgrading to Jasmine 2.x requires a number of test code changes.
    
    Note that firefox-launcher is used for travis CI builds.

--- karma.conf.js
+++ karma.conf.js
@@ -1,58 +1,72 @@
+// Karma configuration
+// Generated on Sat Mar 28 2015 11:17:34 GMT-0700 (PDT)
 
-// base path, that will be used to resolve files and exclude
-basePath = '.';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'misc/test-lib/jquery-1.8.2.min.js',
-  'misc/test-lib/angular.js',
-  'misc/test-lib/angular-mocks.js',
-  'misc/test-lib/helpers.js',
-  'src/**/*.js',
-  'template/**/*.js'
-];
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
-// list of files to exclude
-exclude = [
-  'src/**/docs/*'
-];
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari
-// - PhantomJS
-browsers = [
-  'Chrome'
-];
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
-// test results reporter to use
-// possible values: dots || progress
-reporters = ['progress'];
 
-reportSlowerThan = 100;
+    // list of files / patterns to load in the browser
+    files: [
+      'misc/test-lib/jquery-1.8.2.min.js',
+      'misc/test-lib/angular.js',
+      'misc/test-lib/angular-mocks.js',
+      'misc/test-lib/helpers.js',
+      'src/**/*.js',
+      'template/**/*.js'
+    ],
 
-// web server port
-port = 9018;
 
-// cli runner port
-runnerPort = 9100;
+    // list of files to exclude
+    exclude: [
+      'src/**/docs/*'
+    ],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
 
+    reportSlowerThan: 100,
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
