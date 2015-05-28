a469fc36f28f29152f721eb62ff160a50f3634e0
Robin van Baalen <robin@neverwoods.com>
2015-05-28 13:05:46 -0400

    docs(typeahead): remove hardcoded http protocol
    This prevents protocol mismatch errors
    
    Closes #3723

--- src/typeahead/docs/demo.js
+++ src/typeahead/docs/demo.js
@@ -4,7 +4,7 @@ angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope,
   $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
   // Any function returning a promise object can be used to load values asynchronously
   $scope.getLocation = function(val) {
-    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+    return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
       params: {
         address: val,
         sensor: false
