8ae1a76dc139d8a61ebe368cc11bb462587fba76
Wesley Cho <wesley.cho@gmail.com>
2015-07-20 05:29:48 -0700

    chore(bindHtml): add deprecation notice
    - Add deprecation notice for bindHtmlUnsafe
    
    Closes #3974
    Closes #3574

--- src/bindHtml/bindHtml.js
+++ src/bindHtml/bindHtml.js
@@ -1,10 +1,15 @@
 angular.module('ui.bootstrap.bindHtml', [])
 
-  .directive('bindHtmlUnsafe', function () {
+  .value('$bindHtmlUnsafeSuppressDeprecated', false)
+
+  .directive('bindHtmlUnsafe', ['$log', '$bindHtmlUnsafeSuppressDeprecated', function ($log, $bindHtmlUnsafeSuppressDeprecated) {
     return function (scope, element, attr) {
+      if (!$bindHtmlUnsafeSuppressDeprecated) {
+        $log.warn('bindHtmlUnsafe is now deprecated. Use ngBindHtml instead');
+      }
       element.addClass('ng-binding').data('$binding', attr.bindHtmlUnsafe);
       scope.$watch(attr.bindHtmlUnsafe, function bindHtmlUnsafeWatchAction(value) {
         element.html(value || '');
       });
     };
-  });
+  }]);
