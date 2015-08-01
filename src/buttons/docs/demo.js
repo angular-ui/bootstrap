6b90e9edf2394d5590fd2f6141797d41711db496
Wesley Cho <wesley.cho@gmail.com>
2015-08-01 08:56:05 -0700

    chore(buttons): add demonstration of multiple checkbox
    - Add demonstration of how to store model when working with multiple checkboxes
    
    Closes #3087
    Closes #4065

--- src/buttons/docs/demo.js
+++ src/buttons/docs/demo.js
@@ -8,4 +8,15 @@ angular.module('ui.bootstrap.demo').controller('ButtonsCtrl', function ($scope) 
     middle: true,
     right: false
   };
+
+  $scope.checkResults = [];
+
+  $scope.$watchCollection('checkModel', function () {
+    $scope.checkResults = [];
+    angular.forEach($scope.checkModel, function (value, key) {
+      if (value) {
+        $scope.checkResults.push(key);
+      }
+    });
+  });
 });
