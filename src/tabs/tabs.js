8ae664fa2ef9d27aa6d851e161d945d03e9e1ad1
Wesley Cho <wesley.cho@gmail.com>
2015-09-09 04:13:48 -0400

    chore(tabs): remove `disabled` support
    - Remove deprecated `disabled` support
    
    Closes #4383

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -186,7 +186,7 @@ angular.module('ui.bootstrap.tabs', [])
   </file>
 </example>
  */
-.directive('tab', ['$parse', '$log', function($parse, $log) {
+.directive('tab', ['$parse', function($parse) {
   return {
     require: '^tabset',
     restrict: 'EA',
@@ -213,17 +213,6 @@ angular.module('ui.bootstrap.tabs', [])
       scope.disabled = false;
       if (attrs.disable) {
         scope.$parent.$watch($parse(attrs.disable), function(value) {
-          scope.disabled = !! value;
-        });
-      }
-
-      // Deprecation support of "disabled" parameter
-      // fix(tab): IE9 disabled attr renders grey text on enabled tab #2677
-      // This code is duplicated from the lines above to make it easy to remove once
-      // the feature has been completely deprecated
-      if (attrs.disabled) {
-        $log.warn('Use of "disabled" attribute has been deprecated, please use "disable"');
-        scope.$parent.$watch($parse(attrs.disabled), function(value) {
           scope.disabled = !! value;
         });
       }
