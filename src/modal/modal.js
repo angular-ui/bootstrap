7675457a0625ea7631a777fef1f91de923dd81ae
Wesley Cho <wesley.cho@gmail.com>
2015-10-08 08:00:13 -0700

    chore(modal): remove unnecessary restrict options

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -67,7 +67,6 @@ angular.module('ui.bootstrap.modal', ['ui.bootstrap.stackedMap'])
     }
 
     return {
-      restrict: 'EA',
       replace: true,
       templateUrl: 'template/modal/backdrop.html',
       compile: function(tElement, tAttrs) {
@@ -110,7 +109,6 @@ angular.module('ui.bootstrap.modal', ['ui.bootstrap.stackedMap'])
     }
 
     return {
-      restrict: 'EA',
       scope: {
         index: '@'
       },
@@ -709,7 +707,6 @@ angular.module('ui.bootstrap.modal')
       }
 
       return {
-        restrict: 'EA',
         replace: true,
         templateUrl: 'template/modal/backdrop.html',
         compile: function(tElement, tAttrs) {
@@ -755,7 +752,6 @@ angular.module('ui.bootstrap.modal')
       }
 
       return {
-        restrict: 'EA',
         scope: {
           index: '@'
         },
