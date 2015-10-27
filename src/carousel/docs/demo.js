a1e06861622c8fdb1e3b5c97cae2a83498a6e704
Foxandxss <foxandxss@gmail.com>
2015-10-27 14:29:43 +0100

    chore(carousel): add more options to the demo
    Closes #4749

--- src/carousel/docs/demo.js
+++ src/carousel/docs/demo.js
@@ -1,16 +1,58 @@
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
 angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($scope) {
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
   var slides = $scope.slides = [];
+  var currIndex = 0;
+
   $scope.addSlide = function() {
     var newWidth = 600 + slides.length + 1;
     slides.push({
       image: '//placekitten.com/' + newWidth + '/300',
       text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+      ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4],
+      id: currIndex++
     });
   };
+
+  $scope.randomize = function() {
+    var indexes = generateIndexesArray();
+    assignNewIndexesToSlides(indexes);
+  };
+
   for (var i=0; i<4; i++) {
     $scope.addSlide();
+  }
+
+  // Randomize logic below
+
+  function assignNewIndexesToSlides(indexes) {
+    for (var i = 0, l = slides.length; i < l; i++) {
+      slides[i].id = indexes.pop();
+    }
+  }
+
+  function generateIndexesArray() {
+    var indexes = [];
+    for (var i = 0; i < currIndex; ++i) {
+      indexes[i]=i;
+    }
+    return shuffle(indexes);
+  }
+
+  // http://stackoverflow.com/questions/962802#962890
+  function shuffle(array) {
+    var tmp, current, top = array.length;
+
+    if(top) {
+      while(--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+      }
+    }
+
+    return array;
   }
 });
