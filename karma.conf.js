f6f6db1681be1f1dccb975dc42b7726ea87bf27f
Foxandxss <foxandxss@gmail.com>
2015-10-30 10:31:13 +0100

    chore(karma): add coverage
    Closes #4785

--- karma.conf.js
+++ karma.conf.js
@@ -34,13 +34,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/*/{*.js,!(test)/**/*.js}': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      dir: '.coverage/',
+      type: 'html'
+    },
 
     reportSlowerThan: 100,
 
