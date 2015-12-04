75b8e5aa0e2e88fed18214b75b3d25be5d34c47d
Foxandxss <foxandxss@gmail.com>
2015-12-04 18:52:30 +0100

    refactor(modal): mix both modal spec files
    Closes #5011

--- src/modal/test/modal.spec.js
+++ src/modal/test/modal.spec.js
@@ -1078,6 +1078,44 @@ describe('$uibModal', function () {
     });
   });
 
+  describe('modal window', function() {
+    it('should not use transclusion scope for modals content - issue 2110', function() {
+      $rootScope.animate = false;
+      $compile('<div uib-modal-window animate="animate"><span ng-init="foo=true"></span></div>')($rootScope);
+      $rootScope.$digest();
+
+      expect($rootScope.foo).toBeTruthy();
+    });
+
+    it('should support custom CSS classes as string', function() {
+      $rootScope.animate = false;
+      var windowEl = $compile('<div uib-modal-window animate="animate" window-class="test foo">content</div>')($rootScope);
+      $rootScope.$digest();
+
+      expect(windowEl).toHaveClass('test');
+      expect(windowEl).toHaveClass('foo');
+    });
+
+    it('should support window top class', function () {
+      $rootScope.animate = false;
+      var windowEl = $compile('<div uib-modal-window animate="animate" window-top-class="test foo">content</div>')($rootScope);
+      $rootScope.$digest();
+
+      expect(windowEl).toHaveClass('test');
+      expect(windowEl).toHaveClass('foo');
+    });
+
+    it('should support custom template url', inject(function($templateCache) {
+      $templateCache.put('window.html', '<div class="mywindow" ng-transclude></div>');
+
+      var windowEl = $compile('<div uib-modal-window template-url="window.html" window-class="test">content</div>')($rootScope);
+      $rootScope.$digest();
+
+      expect(windowEl).toHaveClass('mywindow');
+      expect(windowEl).toHaveClass('test');
+    }));
+  });
+
   describe('multiple modals', function() {
     it('it should allow opening of multiple modals', function() {
       var modal1 = open({template: '<div>Modal1</div>'});
