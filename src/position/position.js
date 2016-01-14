ea542213bba67446ee95c0bcac26291d13fd98a7
robjacobs <rob.jacobs@msn.com>
2016-01-14 13:10:20 -0500

    Revert "Merge branch 'position-fix' of https://github.com/RobJacobs/bootstrap"
    This reverts commit a25caa7c0052ef755bbed6019c70fc40de57129a, reversing
    changes made to 00ac93e3ecc7549c431ad519995e645d71f26368.

--- src/position/position.js
+++ src/position/position.js
@@ -464,12 +464,16 @@ angular.module('ui.bootstrap.position', [])
       positionArrow: function(elem, placement) {
         elem = this.getRawNode(elem);
 
-        var innerElem = elem.querySelector('.tooltip-inner, .popover-inner');
+        var isTooltip = true;
+
+        var innerElem = elem.querySelector('.tooltip-inner');
+        if (!innerElem) {
+          isTooltip = false;
+          innerElem = elem.querySelector('.popover-inner');
+        }
         if (!innerElem) {
           return;
         }
-
-        var isTooltip = angular.element(innerElem).hasClass('tooltip-inner');
 
         var arrowElem = isTooltip ? elem.querySelector('.tooltip-arrow') : elem.querySelector('.arrow');
         if (!arrowElem) {
