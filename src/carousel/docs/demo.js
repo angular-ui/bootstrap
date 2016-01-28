0b7e742f6e5132f70afe59c687f8625b77b36940
Wesley Cho <wesley.cho@gmail.com>
2016-01-28 06:19:35 -0800

    docs(carousel): make sure request is made to http
    - Ensure request uses http protocol and not https potentially
    
    Closes #5359

--- src/carousel/docs/demo.js
+++ src/carousel/docs/demo.js
@@ -7,7 +7,7 @@ angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($sc
   $scope.addSlide = function() {
     var newWidth = 600 + slides.length + 1;
     slides.push({
-      image: '//lorempixel.com/' + newWidth + '/300',
+      image: 'http://lorempixel.com/' + newWidth + '/300',
       text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
       id: currIndex++
     });
