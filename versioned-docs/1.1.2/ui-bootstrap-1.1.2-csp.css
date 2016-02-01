315ba26cad5fcd5b4732005b1e1e244746338339
Wesley Cho <wesley.cho@gmail.com>
2016-02-01 09:04:21 -0800

    chore(release): add files

---
+++ versioned-docs/1.1.2/ui-bootstrap-1.1.2-csp.css
@@ -0,0 +1,29 @@
+/* Include this file in your html if you are using the CSP mode. */
+
+.ng-animate.item:not(.left):not(.right) {
+  -webkit-transition: 0s ease-in-out left;
+  transition: 0s ease-in-out left
+}
+.uib-datepicker .uib-title {
+  width: 100%;
+}
+
+.uib-day button, .uib-month button, .uib-year button {
+  min-width: 100%;
+}
+
+.uib-datepicker-popup.dropdown-menu {
+  display: block;
+}
+
+.uib-button-bar {
+  padding: 10px 9px 2px;
+}
+
+.uib-time input {
+  width: 50px;
+}
+
+[uib-typeahead-popup].dropdown-menu {
+  display: block;
+}
