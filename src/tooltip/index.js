c8922a2f04b7ac79c552770281226bd5b99783e1
Matt Lewis <mattlewis92@users.noreply.github.com>
2016-02-29 11:27:34 +0000

    fix(tooltip): import CSS
    - Import the CSS in the tooltip index.js
    
    Closes #5552

--- src/tooltip/index.js
+++ src/tooltip/index.js
@@ -5,6 +5,8 @@ require('../../template/tooltip/tooltip-html-popup.html.js');
 require('../../template/tooltip/tooltip-template-popup.html.js');
 require('./tooltip');
 
+require('./tooltip.css');
+
 var MODULE_NAME = 'ui.bootstrap.module.tooltip';
 
 angular.module(MODULE_NAME, ['ui.bootstrap.tooltip', 'uib/template/tooltip/tooltip-popup.html', 'uib/template/tooltip/tooltip-html-popup.html', 'uib/template/tooltip/tooltip-template-popup.html']);
