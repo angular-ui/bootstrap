48f62a6dadaee110a4d6777088a20681878dd407
Foxandxss <foxandxss@gmail.com>
2016-03-27 19:05:25 +0200

    chore(timepicker): remove css require on noncss

--- src/timepicker/index-nocss.js
+++ src/timepicker/index-nocss.js
@@ -1,8 +1,6 @@
 require('../../template/timepicker/timepicker.html.js');
 require('./timepicker');
 
-require('./timepicker.css');
-
 var MODULE_NAME = 'ui.bootstrap.module.timepicker';
 
 angular.module(MODULE_NAME, ['ui.bootstrap.timepicker', 'uib/template/timepicker/timepicker.html']);
