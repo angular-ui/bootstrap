4fef0377bb962c365914b257122876f6911a56d3
cherry-geqi <xian.tuxoid@qq.com>
2016-06-03 10:49:15 +0800

    fix(datepickerPopup): specify dependency on datepicker
    - Specify dependency on datepicker module
    
    Closes #5954

--- src/datepickerPopup/index-nocss.js
+++ src/datepickerPopup/index-nocss.js
@@ -5,6 +5,6 @@ require('./popup.js');
 
 var MODULE_NAME = 'ui.bootstrap.module.datepickerPopup';
 
-angular.module(MODULE_NAME, ['ui.bootstrap.datepickerPopup', 'uib/template/datepickerPopup/popup.html']);
+angular.module(MODULE_NAME, ['ui.bootstrap.datepickerPopup', 'uib/template/datepickerPopup/popup.html', 'ui.bootstrap.module.datepicker']);
 
 module.exports = MODULE_NAME;
