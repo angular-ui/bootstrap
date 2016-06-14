164811ab356b833cd55102297b6d8dcb678d18dd
Andrij Fedyk <fedyk72@gmail.com>
2016-06-14 10:14:39 +0200

    fix(tooltip): missed dependency for cjs
    Closes #5999

--- src/modal/index-nocss.js
+++ src/modal/index-nocss.js
@@ -5,6 +5,6 @@ require('./modal');
 
 var MODULE_NAME = 'ui.bootstrap.module.modal';
 
-angular.module(MODULE_NAME, ['ui.bootstrap.modal', 'uib/template/modal/backdrop.html', 'uib/template/modal/window.html']);
+angular.module(MODULE_NAME, ['ui.bootstrap.modal', 'uib/template/modal/window.html']);
 
 module.exports = MODULE_NAME;
