1a132dd7573ae23784d4b62cb5eff778ff3514ef
Evan Theurer <theurere@gmail.com>
2016-08-31 16:23:31 +0200

    docs(collapse): add navigation collapse example
    - Add an example for using collapse with the bootstrap navbar
    
    Closes #3481
    Closes #6217

--- src/collapse/docs/demo.js
+++ src/collapse/docs/demo.js
@@ -1,4 +1,5 @@
 angular.module('ui.bootstrap.demo').controller('CollapseDemoCtrl', function ($scope) {
+  $scope.isNavCollapsed = true;
   $scope.isCollapsed = false;
   $scope.isCollapsedHorizontal = false;
 });
